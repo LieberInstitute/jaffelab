8d4e58341d1202e258a53cceed9015471c8fd932
lcolladotor <lcollado@jhsph.edu>
2016-10-05 14:11:32 -0400

    Add getPcaVars()

---
+++ R/getPcaVars.R
@@ -0,0 +1,24 @@
+#' Calculates the percent of variance explained for principal components
+#'
+#' Given a PCA object created with \link[stats]{prcomp}, this function computes 
+#' the percent of variance explained by each of the principal components.
+#'
+#' @param pca An object created with \link[stats]{prcomp}.
+#' @param digits The number of significant digits to round to.
+#'
+#' @return A vector with the percent of variance explained for each of the
+#' principal components in decreasing order.
+#'
+#' @export
+#' @author Andrew E Jaffe
+#'
+#' @examples
+#'
+#' pca <- prcomp(USArrests)
+#' getPcaVars(pca)
+#'
+
+getPcaVars <- function(pca, digits = 3) {
+    stopifnot(is(pca) == 'prcomp')
+    signif(pca$sdev^2 / sum(pca$sdev^2) * 100, digits = digits)
+}
---
+++ tests/testthat/test_pca.R
@@ -0,0 +1,7 @@
+context('PCA')
+
+pca <- prcomp(USArrests)
+test_that('getPcaVars', {
+    expect_equal(getPcaVars(pca)[1], 96.6)
+    expect_equal(getPcaVars(pca, digits = 5)[2], 2.7817)
+})
